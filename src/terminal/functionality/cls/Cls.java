e0268f066343ea14838eec6421b667360afb024d
Greg <gregoryamitten@gmail.com>
2017-07-15 13:47:11 +0100

    refactored packaging

---
+++ src/terminal/functionality/cls/Cls.java
@@ -0,0 +1,7 @@
+package terminal.functionality.cls;
+
+/**
+ * Created by i7676925 on 15/07/2017.
+ */
+public class Cls {
+}
---
+++ src/terminal/functionality/streams/Streams.java
@@ -0,0 +1,10 @@
+package terminal.functionality.streams;
+
+/**
+ * Created by i7676925 on 15/07/2017.
+ */
+//TODO register a callback URI so you can access their API instead
+public class TwitchWebScraper {
+
+}
+//password thisisabot
