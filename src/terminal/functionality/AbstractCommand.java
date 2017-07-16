7283356afa545338edd46181b722a4ee9a9f85e9
Greg <gregoryamitten@gmail.com>
2017-07-16 12:17:33 +0100

    attempting to implement generics into the new command classes

---
+++ src/terminal/functionality/AbstractCommand.java
@@ -0,0 +1,7 @@
+package terminal.functionality;
+
+/**
+ * Created by i7676925 on 16/07/2017.
+ */
+public class AbstractCommand {
+}
---
+++ src/terminal/functionality/twitch/Twitch.java
@@ -0,0 +1,7 @@
+package terminal.functionality.twitch;
+
+/**
+ * Created by i7676925 on 16/07/2017.
+ */
+public class Twitch {
+}
