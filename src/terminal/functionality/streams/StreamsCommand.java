167382e0d88b64b61cc00e1108c13b5fe77d13dd
Greg <gregoryamitten@gmail.com>
2017-07-18 12:56:55 +0100

    Attempting to use twitch api, having some dependancy issues

--- src/terminal/functionality/streams/StreamsCommand.java
+++ src/terminal/functionality/streams/StreamsCommand.java
@@ -1,11 +1,16 @@
 package terminal.functionality.streams;
 
-import com.jaunt.*;
-import com.jaunt.component.Form;
+import com.jaunt.ResponseException;
+import com.jaunt.SearchException;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
 import terminal.functionality.AbstractCommand;
-import terminal.functionality.Command;
 import terminal.functionality.Console;
-import terminal.ui.UIController;
+
+import java.io.IOException;
 
 /**
  * Created by i7676925 on 15/07/2017.
@@ -16,22 +21,18 @@ public class StreamsCommand extends AbstractCommand {
     private final String username = "GregShellBot";
     private final String password = "thisisabot";
 
+    //https://api.twitch.tv/kraken/users/GregShellBot/follows/channels?client_id=2oibbr9895k1w9dd6ke5in4l0st6p7
+
 
     @Override
-    public void execute(String parameters) throws ResponseException, SearchException {
-        UserAgent userAgent = new UserAgent();
-        userAgent.visit("http://twitch.tv/login");
-
-        Form loginForm = userAgent.doc.getForm(0);
-
-        loginForm.fillout("username", username);
-        loginForm.fillout("password", password);
-        loginForm.submit();
+    public void execute(String parameters) throws ResponseException, SearchException, IOException, UnirestException {
+        HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.get("https://api.twitch.tv/kraken/streams/followed")
+                .header("Accept", "application/vnd.twitchtv.v5+json")
+                .header("Client-Id", "2oibbr9895k1w9dd6ke5in4l0st6p7")
+                .header("Authorization", "oauth:u224dzzeqn66yvawjs6jl8e4muru7o")
+                .asJson();
         System.out.println();
-    }
 
-    @Override
-    public void setUp() {
-        Console.registerCommand(this.getClass().getSimpleName(), this);
+
     }
 }
