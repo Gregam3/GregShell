5ecabd5117ec1eb5f6144b3cf9fcde4098bd5c38
Greg <gregoryamitten@gmail.com>
2017-08-24 13:44:13 +0100

    testing new git command in gregshell

--- src/main/java/terminal/functionality/quickpush/QuickPushCommand.java
+++ src/main/java/terminal/functionality/quickpush/QuickPushCommand.java
@@ -1,7 +1,7 @@
 package terminal.functionality.quickpush;
 
-        import terminal.functionality.AbstractCommand;
-        import terminal.functionality.Console;
+import terminal.functionality.AbstractCommand;
+import terminal.functionality.Console;
 
 /**
  * Created by Greg Mitten on 24/08/2017.
@@ -10,7 +10,11 @@ public class QuickPushCommand extends AbstractCommand {
     @Override
     public void execute(String parameters) throws Exception {
         Runtime.getRuntime().exec(
-                "cmd /c start cmd.exe /K \"cd C:/Users/i7676925/IdeaProjects/GregShell && git add -A &&git commit -m \"test push for new push command\" && git push \""
+                "cmd /c start cmd.exe /K " +
+                        "\"cd C:/Users/i7676925/IdeaProjects/GregShell && " +
+                        "git add -A && " +
+                        "git commit -m \"" + parameters +"\" && " +
+                        "git push \""
         );
     }
 }
